b29918a97412f4c0e058ee0517facd0fd6aef4d1
William-Boomer-Baker <boomer@boomerbaker.com>
2023-02-24 23:23:53 +0530

    test stripe bug fix

--- server/submit.js
+++ server/submit.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 require('./customFunctions/userModel')
 const User = mongoose.model('emails')
+const shortid = require("shortid");
 
 exports.handler = async (event, context) => {
   const array = event.body.split('email=')
@@ -20,8 +21,11 @@ exports.handler = async (event, context) => {
     }
 
     if (!existingUser) {
+      const shortIdVariable = shortid.generate();
       const user = await new User({
         email: email,
+        referralId: shortIdVariable,
+        numberOfReferrals: 0
       }).save()
     }
     mongoose.disconnect()
